b9e1b38595d0fc84fd1188c550a822fdcd17786e
babogyeong <jbk021010@gmail.com>
2023-07-31 04:39:26 +0900

    처음 실행화면(3초동안 보여주게 함), 메인화면, 로그인, 회원가입창 만들었음, db연결함 추후 디자인 보충 예정, 로그인 연결제대로 안 되는듯

---
+++ app/src/main/java/com/example/guru30realreal_app/auth/IntroActivity.kt
@@ -0,0 +1,27 @@
+package com.example.guru30realreal_app.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.guru30realreal_app.R
+import com.example.guru30realreal_app.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
