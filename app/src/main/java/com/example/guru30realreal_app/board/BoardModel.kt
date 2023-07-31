d150424bf92be887605c3ad65201189d59c0de63
babogyeong <jbk021010@gmail.com>
2023-07-31 20:08:24 +0900

    게시판..만드는중..

---
+++ app/src/main/java/com/example/guru30realreal_app/board/BoardModel.kt
@@ -0,0 +1,9 @@
+package com.example.guru30realreal_app.board
+
+data class BoardModel (
+    val title : String = "",
+    val content : String = "",
+    val uid : String = "",
+    val time : String = ""
+
+        )
