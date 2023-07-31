ea30ca1efe59731acd641e4a604350087e5627db
Lee Chaeyoung <0929alice@naver.com>
2023-07-31 18:55:19 +0900

    teamproject->guru30realreal_app

--- app/src/main/java/com/example/guru30realreal_app/SplashActivity.kt
+++ app/src/main/java/com/example/guru30realreal_app/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.guru30realreal_app.auth.IntroActivity
+import com.example.guru30realreal_app.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
